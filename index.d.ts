d5dbe5ee659f1a2436e0a3cb1dee7d76f5df42e3
Maya <maya@f-list.net>
2020-10-01 21:37:08 +0200

    v2.0.0 - Vue 3 support

--- index.d.ts
+++ index.d.ts
@@ -1,12 +1,19 @@
-import Vue, {ComponentOptions, PropOptions, WatchOptions} from 'vue';
+import {ComponentOptions, Prop as PropOptions, WatchOptions, ComponentPublicInstance} from 'vue';
 type Constructor<T> = Function & { prototype: T }
-export declare function Component(options: ComponentOptions<Vue>): <V extends Constructor<Vue>>(target: V) => V;
-export declare function Component<V extends Constructor<Vue>>(target: V): V;
+interface Vue extends ComponentPublicInstance {}
+declare class Vue implements ComponentPublicInstance {}
 
-declare const Prop: PropertyDecorator & ((options?: PropOptions) => PropertyDecorator);
+type Hooks = 'beforeCreate' | 'created' | 'beforeMount' | 'mounted' | 'beforeUpdate' | 'updated' | 'activated' | 'deactivated' | 'beforeUnmount' | 'unmounted' | 'errorCaptured' | 'renderTracked' | 'renderTriggered';
+
+export declare function Component<V extends Constructor<Vue>>(target: V): V;
+export declare function Component(options: ComponentOptions): <V extends Constructor<Vue>>(target: V) => V;
+
+declare const Prop: PropertyDecorator & ((options: PropOptions<any>) => PropertyDecorator);
 
 export declare function Watch(expression: string, options?: WatchOptions): MethodDecorator;
 
-export declare function Hook(name: 'beforeCreate' | 'created' | 'beforeMount' | 'mounted' | 'beforeDestroy' | 'destroyed' | 'beforeUpdate' | 'updated' | 'activated' | 'deactivated'): MethodDecorator;
+export declare function Hook(name: Hooks): MethodDecorator;
 
 export {Prop}
+export default Vue;
+
--- transform.ts
+++ transform.ts
@@ -1,109 +1,107 @@
 import * as ts from 'typescript';
+import {ObjectLiteralExpression} from 'typescript';
 
 function getDecoratorName(decorator: ts.Decorator) {
-    return ts.isCallExpression(decorator.expression) ? decorator.expression.expression.getText(decorator.getSourceFile()) : decorator.expression.getText(decorator.getSourceFile());
+	return ts.isCallExpression(decorator.expression) ? decorator.expression.expression.getText(decorator.getSourceFile()) : decorator.expression.getText(decorator.getSourceFile());
 }
 
 function getDecoratorArgument(decorator: ts.Decorator, index: number) {
-    return ts.isCallExpression(decorator.expression) ? decorator.expression.arguments[index] : undefined;
-}
-
-function createProperty(object: ts.ObjectLiteralExpression, expr: ts.ObjectLiteralElementLike) {
-    (<ts.ObjectLiteralElementLike[]><unknown>object.properties).push(expr);
-}
-
-function copyIfObject(object: ts.Node | undefined) {
-    return ts.createObjectLiteral(object && ts.isObjectLiteralExpression(object) ? object.properties : undefined);
+	return ts.isCallExpression(decorator.expression) ? decorator.expression.arguments[index] : undefined;
 }
 
 const transformer: ts.TransformerFactory<ts.SourceFile> = (context) => {
-    const visitor: ts.Visitor = (node) => {
-        const decorator = node.decorators && node.decorators.filter(x => getDecoratorName(x) === 'Component')[0];
-        if(decorator) {
-            const data = copyIfObject(getDecoratorArgument(decorator, 0));
-            const computed: {[key: string]: {get?: ts.AccessorDeclaration, set?: ts.AccessorDeclaration}} = {}, watch: {[key: string]: ts.ObjectLiteralExpression[]} = {}, hooks: {[key: string]: ts.Expression[]} = {};
-            const methods = ts.createObjectLiteral(), props = ts.createObjectLiteral();
-            createProperty(data, ts.createPropertyAssignment('methods', methods));
-            createProperty(data, ts.createPropertyAssignment('props', props));
-            const dataObj = ts.createObjectLiteral();
-            createProperty(data, ts.createMethod(undefined, undefined, undefined, 'data', undefined, undefined, [], undefined, ts.createBlock([ts.createReturn(dataObj)])));
-            const cls = <ts.ClassDeclaration>node;
-            const base = cls.heritageClauses!.filter(x => x.token == ts.SyntaxKind.ExtendsKeyword)[0].types[0];
-            for(const member of cls.members) {
-                if(!member.decorators) member.decorators = ts.createNodeArray();
-                if(member.modifiers && member.modifiers.some(x => x.kind === ts.SyntaxKind.AbstractKeyword)) continue;
-                if(ts.isAccessor(member)) {
-                    const entry = computed[member.name!.getText()] || (computed[member.name!.getText()] = {});
-                    entry[ts.isGetAccessor(member) ? 'get' : 'set'] = member;
-                } else if(ts.isPropertyDeclaration(member)) {
-                    const prop = member.decorators!.filter(x => getDecoratorName(x) === 'Prop')[0];
-                    if(prop) {
-                        const propData = copyIfObject(getDecoratorArgument(prop, 0));
-                        //if(property.type)
-                        //    createProperty(propData, ts.createPropertyAssignment('type', ts.createIdentifier(property.type.getText())));
-                        createProperty(props, ts.createPropertyAssignment(member.name, propData));
-                        continue;
-                    }
-                    if(member.name.getText().startsWith('$')) continue;
-                    createProperty(dataObj, ts.createPropertyAssignment(member.name, (<ts.PropertyDeclaration>member).initializer || ts.createIdentifier('undefined')))
-                } else if(ts.isMethodDeclaration(member)) {
-                    function replaceIfSuper(node: ts.Node) {
-                        if((ts.isPropertyAccessExpression(node) || ts.isElementAccessExpression(node)) && node.expression.kind === ts.SyntaxKind.SuperKeyword) {
-                            if(!ts.isCallExpression(node.parent))
-                                throw new Error('The super keyword is only supported in call expressions.');
-                            node.expression = ts.createPropertyAccess(ts.createPropertyAccess(base.expression, 'options'), 'methods');
-                            node.parent.expression = ts.createPropertyAccess(node, 'call');
-                            (<ts.Expression[]><unknown>node.parent.arguments).unshift(ts.createThis());
-                        } else ts.forEachChild(node, replaceIfSuper);
-                    }
-                    ts.forEachChild(member, replaceIfSuper);
-                    createProperty(methods, member);
-                    const hookDecorators = member.decorators!.filter(x => getDecoratorName(x) === 'Hook');
-                    for(const hook of hookDecorators) {
-                        const name = (<ts.StringLiteral>getDecoratorArgument(hook, 0)).text;
-                        const entry = hooks[name] || (hooks[name] = []);
-                        entry.push(ts.isLiteralExpression(member.name) ? member.name : ts.isIdentifier(member.name) ? ts.createLiteral(member.name) : member.name.expression);
-                    }
-                    const watches = member.decorators!.filter(x => getDecoratorName(x) === 'Watch');
-                    for(const watchDecorator of watches) {
-                        const watchData = copyIfObject(getDecoratorArgument(watchDecorator, 1));
-                        createProperty(watchData, ts.createPropertyAssignment(ts.createIdentifier('handler'), ts.createStringLiteral(member.name.getText())))
-                        const name = (<ts.StringLiteral>getDecoratorArgument(watchDecorator, 0)).text;
-                        const entry = watch[name] || (watch[name] = []);
-                        entry.push(watchData);
-                    }
-                    member.decorators = undefined;
-                }
-            }
+	const visitor: ts.Visitor = (node) => {
+		const f = context.factory;
+		const decorator = node.decorators?.filter(x => getDecoratorName(x) === 'Component')[0];
+		if(!decorator) return node;
 
-            function createIfAny<T>(entries: {[key: string]: T}, name: string, iterator: (key: string, value: T) => ts.Expression) {
-                const keys = Object.keys(entries);
-                if(!keys.length) return;
-                createProperty(data, ts.createPropertyAssignment(name, ts.createObjectLiteral(keys.map(x => ts.createPropertyAssignment(ts.createStringLiteral(x), iterator(x, entries[x]))))));
-            }
-            createIfAny(computed, 'computed', (key, value) => {
-                if(!value.get) throw new Error("No getter defined for " + key);
-                const prop = ts.createObjectLiteral([ts.createMethod(undefined, undefined, undefined, 'get', undefined, undefined, [], undefined, value.get.body)]);
-                if(value.set)
-                    createProperty(prop, ts.createMethod(undefined, undefined, undefined, 'set', undefined, undefined, value.set.parameters, undefined, value.set.body))
-                return prop;
-            })
-            createIfAny(watch, 'watch', (_, value) => ts.createArrayLiteral(value));
-            for(const hook in hooks) {
-                const block = hooks[hook].map(x => ts.createExpressionStatement(ts.createCall(ts.createPropertyAccess(ts.createElementAccess(ts.createThis(), x), 'apply'), undefined, [ts.createThis(), ts.createIdentifier('arguments')])))
-                createProperty(data, ts.createMethod(undefined, undefined, undefined, hook, undefined, undefined, [], undefined, ts.createBlock(block)));
-            }
+		const computed: {[key: string]: {get?: ts.AccessorDeclaration, set?: ts.AccessorDeclaration}} = {},
+			watch: {[key: string]: ts.ObjectLiteralExpression[]} = {}, hooks: {[key: string]: ts.Expression[]} = {};
+		const cls = <ts.ClassDeclaration>node;
+		const base = cls.heritageClauses!.filter(x => x.token == ts.SyntaxKind.ExtendsKeyword)[0].types[0];
+		const data: ts.PropertyAssignment[] = [], props: ts.PropertyAssignment[] = [], methods: ts.MethodDeclaration[] = [];
+		for(const member of cls.members) {
+			if(member.modifiers?.some(x => x.kind === ts.SyntaxKind.AbstractKeyword)) continue;
+			if(ts.isAccessor(member)) {
+				const entry = (computed[member.name!.getText()] ??= {});
+				entry[ts.isGetAccessor(member) ? 'get' : 'set'] = member;
+			} else if(ts.isPropertyDeclaration(member)) {
+				const prop = member.decorators?.filter(x => getDecoratorName(x) === 'Prop')[0];
+				if(prop)
+					props.push(f.createPropertyAssignment(member.name, getDecoratorArgument(prop, 0) || f.createObjectLiteralExpression()));
+				else
+					data.push(f.createPropertyAssignment(member.name, member.initializer || f.createIdentifier('undefined')));
+			} else if(ts.isMethodDeclaration(member)) {
+				function replaceIfSuper(node: ts.Node) {
+					if((ts.isPropertyAccessExpression(node) || ts.isElementAccessExpression(node)) && node.expression.kind === ts.SyntaxKind.SuperKeyword) {
+						if(!ts.isCallExpression(node.parent))
+							throw new Error('The super keyword is only supported in call expressions.');
+						node.expression = ts.createPropertyAccess(ts.createPropertyAccess(base.expression, 'options'), 'methods');
+						node.parent.expression = ts.createPropertyAccess(node, 'call');
+						(<ts.Expression[]><unknown>node.parent.arguments).unshift(ts.createThis());
+					} else ts.forEachChild(node, replaceIfSuper);
+				}
 
-            return [
-                ts.createVariableStatement([ts.createModifier(ts.SyntaxKind.ConstKeyword)],
-                    [ts.createVariableDeclaration(cls.name!, undefined, ts.createCall(ts.createPropertyAccess(base.expression, ts.createIdentifier('extend')), undefined, [data]))]),
-                ts.createExportDefault(cls.name!)
-            ];
-        }
-        return node
-    };
+				ts.forEachChild(member, replaceIfSuper);
+				methods.push(member);
+				const hookDecorators = member.decorators?.filter(x => getDecoratorName(x) === 'Hook');
+				for(const hook of hookDecorators || []) {
+					const name = (<ts.StringLiteral>getDecoratorArgument(hook, 0)).text;
+					const entry = (hooks[name] ??= []);
+					entry.push(ts.isLiteralExpression(member.name) ? member.name : ts.isIdentifier(member.name) ? ts.createLiteral(member.name) : member.name.expression);
+				}
+				const watches = member.decorators?.filter(x => getDecoratorName(x) === 'Watch');
+				for(const watchDecorator of watches || []) {
+					const existing = (getDecoratorArgument(watchDecorator, 1) as ObjectLiteralExpression)?.properties || [];
+					const data = f.createObjectLiteralExpression([...existing, f.createPropertyAssignment(f.createIdentifier('handler'), f.createStringLiteral(member.name.getText()))]);
+					const name = (<ts.StringLiteral>getDecoratorArgument(watchDecorator, 0)).text;
+					(watch[name] ??= []).push(data);
+				}
+				member.decorators = undefined;
+			}
+		}
 
-    return (node) => ts.visitEachChild(node, visitor, context);
+		const options = [...((getDecoratorArgument(decorator, 0) as ts.ObjectLiteralExpression)?.properties) || []];
+		if(base.expression.getText() !== 'Vue') {
+			let mixins = options.find(x => x.name?.getText() === 'mixins');
+			if(!mixins) {
+				mixins = f.createPropertyAssignment('mixins', f.createArrayLiteralExpression());
+				options.push(mixins);
+			}
+			(((mixins as ts.PropertyAssignment).initializer as ts.ArrayLiteralExpression).elements as unknown as ts.Node[]).push(base.expression);
+		}
+
+		function createIfAny<T>(entries: {[key: string]: T}, name: string, iterator: (key: string, value: T) => ts.Expression) {
+			const keys = Object.keys(entries);
+			if(!keys.length) return;
+			options.push(f.createPropertyAssignment(name, f.createObjectLiteralExpression(keys.map(x => f.createPropertyAssignment(f.createStringLiteral(x), iterator(x, entries[x]))))));
+		}
+
+		createIfAny(computed, 'computed', (key, value) => {
+			if(!value.get) throw new Error('No getter defined for ' + key);
+			const prop = [f.createMethodDeclaration(undefined, undefined, undefined, 'get', undefined, undefined, [], undefined, value.get.body)];
+			if(value.set)
+				prop.push(f.createMethodDeclaration(undefined, undefined, undefined, 'set', undefined, undefined, value.set.parameters, undefined, value.set.body));
+			return f.createObjectLiteralExpression(prop);
+		});
+		createIfAny(watch, 'watch', (_, value) => f.createArrayLiteralExpression(value));
+		for(const hook in hooks) {
+			const block = hooks[hook].map(x => f.createExpressionStatement(f.createCallExpression(f.createPropertyAccessExpression(f.createElementAccessExpression(f.createThis(), x), 'apply'),
+				undefined, [f.createThis(), f.createIdentifier('arguments')])));
+			options.push(f.createMethodDeclaration(undefined, undefined, undefined, hook, undefined, undefined, [], undefined, f.createBlock(block)));
+		}
+		if(props.length) options.push(f.createPropertyAssignment('props', f.createObjectLiteralExpression(props)));
+		if(methods.length) options.push(f.createPropertyAssignment('methods', f.createObjectLiteralExpression(methods)));
+		if(data.length)
+			options.push(f.createMethodDeclaration(undefined, undefined, undefined, 'data', undefined, undefined, [], undefined, f.createBlock([f.createReturnStatement(f.createObjectLiteralExpression(data))])));
+
+		return [
+			f.createVariableStatement([f.createModifier(ts.SyntaxKind.ConstKeyword)], [f.createVariableDeclaration(cls.name!, undefined, undefined, f.createObjectLiteralExpression(options))]),
+			f.createExportDefault(cls.name!)
+		];
+	};
+
+	return (node) => ts.visitEachChild(node, visitor, context);
 };
 
 export default transformer;
